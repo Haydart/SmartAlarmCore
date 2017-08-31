6b331802deffe2c4890f53559487aa5c7c20b818
Haydart <makowiecki.radoslaw@gmail.com>
2017-08-31 23:55:07 +0200

    Debug print network connection state on app launch

--- app/src/main/java/pl/rmakowiecki/smartalarmcore/MainActivity.kt
+++ app/src/main/java/pl/rmakowiecki/smartalarmcore/MainActivity.kt
@@ -1,5 +1,7 @@
 package pl.rmakowiecki.smartalarmcore
 
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        printWifiNetworkStatus()
     }
 
     private fun sendAlarmTriggerData(gpio: Gpio) {
@@ -21,5 +24,14 @@ class MainActivity : AppCompatActivity() {
                 .addOnCompleteListener {
                     Log.d(javaClass.simpleName, if (it.isSuccessful) "Server trigger data push successful" else "Error sending data to server")
                 }
+    }
+
+    private fun printWifiNetworkStatus() {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI)
+
+        if (wifi.isConnected) {
+            Log.d(javaClass.simpleName, wifi.toString())
+        }
     }
 }
