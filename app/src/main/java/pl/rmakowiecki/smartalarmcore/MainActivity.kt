e27a0d358bcf6d142ec1361cc685c6c20e01c312
Haydart <makowiecki.radoslaw@gmail.com>
2017-08-31 23:53:48 +0200

    Add simple alarm triggering funtion

--- app/src/main/java/pl/rmakowiecki/smartalarmcore/MainActivity.kt
+++ app/src/main/java/pl/rmakowiecki/smartalarmcore/MainActivity.kt
@@ -2,10 +2,24 @@ package pl.rmakowiecki.smartalarmcore
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.things.pio.Gpio
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+
+    private fun sendAlarmTriggerData(gpio: Gpio) {
+        FirebaseDatabase
+                .getInstance()
+                .reference
+                .child("trigger")
+                .setValue(gpio.value)
+                .addOnCompleteListener {
+                    Log.d(javaClass.simpleName, if (it.isSuccessful) "Server trigger data push successful" else "Error sending data to server")
+                }
     }
 }
