7fb7d2c208cdd10c5b42e132fc31f79af7ae74b2
Haydart <makowiecki.radoslaw@gmail.com>
2017-09-12 22:10:26 +0200

    Cleanup USB state broadcast receiver code

---
+++ app/src/main/java/pl/rmakowiecki/smartalarmcore/background/UsbStateBroadcastReceiver.kt
@@ -0,0 +1,21 @@
+package pl.rmakowiecki.smartalarmcore.background
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import pl.rmakowiecki.smartalarmcore.extensions.logD
+
+const val USB_STATE_CHANGE_ACTION = "android.hardware.usb.action.USB_STATE"
+const val USB_DEVICE_ATTACHED_ACTION = "android.hardware.usb.action.USB_DEVICE_ATTACHED"
+const val USB_DEVICE_DETACHED_ACTION = "android.hardware.usb.action.USB_DEVICE_DETACHED"
+
+class UsbStateBroadcastReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        when (intent.action) {
+            USB_STATE_CHANGE_ACTION -> logD("USB state changed")
+            USB_DEVICE_ATTACHED_ACTION -> logD("USB device attached")
+            USB_DEVICE_DETACHED_ACTION -> logD("USB device detached")
+        }
+    }
+}
