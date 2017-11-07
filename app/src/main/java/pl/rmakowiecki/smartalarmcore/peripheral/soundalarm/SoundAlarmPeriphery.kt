a84ea25ff755a45ea1284e69f19e684783ff1380
Haydart <makowiecki.radoslaw@gmail.com>
2017-11-07 19:08:59 +0100

    Implement siren alarm periphery

--- app/src/main/java/pl/rmakowiecki/smartalarmcore/peripheral/soundalarm/SoundAlarmPeriphery.kt
+++ app/src/main/java/pl/rmakowiecki/smartalarmcore/peripheral/soundalarm/SoundAlarmPeriphery.kt
@@ -1,12 +1,38 @@
 package pl.rmakowiecki.smartalarmcore.peripheral.soundalarm
 
+import com.google.android.things.pio.Gpio
+import com.google.android.things.pio.PeripheralManagerService
+import pl.rmakowiecki.smartalarmcore.extensions.logE
+
+private const val PIN_NAME = "BCM21"
+
 class SoundAlarmPeriphery {
 
-    fun startSiren() {
+    private lateinit var alarmSirenGpio: Gpio
 
+    init {
+        initAndRegisterGpioCallback()
+    }
+
+    private fun initAndRegisterGpioCallback() {
+        val service = PeripheralManagerService()
+        try {
+            alarmSirenGpio = service.openGpio(PIN_NAME)
+            alarmSirenGpio.apply {
+                setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+            }
+
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+            logE("GPIO exception")
+        }
+    }
+
+    fun startSiren() {
+        alarmSirenGpio.value = true
     }
 
     fun stopSiren() {
-
+        alarmSirenGpio.value = false
     }
 }
